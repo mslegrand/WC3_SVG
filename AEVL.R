dfc944bfc6b64b91dc51bc770c18df555e973e60
mslegrand <legrand@coanalytics.net>
2014-08-16 12:15:30 -0400

    getMissingInfo wraps MissingInfo

--- AEVL.R
+++ AEVL.R
@@ -208,85 +208,116 @@ AVEL2.df$treatValueAs<-treatValueAs
 grep("\\|",AVEL2.df$val)->choiceIndx
 AVEL2.df[choiceIndx,"treatValueAs"]<-"Choice"
 
+getMissingInfo<-function(AVEL2.df){
+  which(is.na(AVEL2.df$treatValueAs))->NA.indx
+  unique(AVEL2.df$val[NA.indx])->missingTypes
+  table(AVEL2.df$val[NA.indx])->missingTypeTable
+  sort(missingTypeTable, decreasing = T)->missingTypeTable
+  missingType.df<-data.frame(val=names(missingTypeTable), treatValueAs=NA)
+  missingType.List<-lapply(names(missingTypeTable), function(x){intersect(NA.indx, which(x==AVEL2.df$val)) })
+  names(missingType.List)<-names(missingTypeTable)
+  missingType.info.df<-lapply(missingType.List, function(x){AVEL2.df[x[[1]],]} )
+  missingType.info.df<-do.call(rbind, missingType.info.df)
+  missingType.info.df$page<-NULL
+  missingType.info.df$loc<-NULL
+  rownames(missingType.info.df)<-NULL
+  missingType.info.df$Example<-NA
 
-which(is.na(AVEL2.df$treatValueAs))->NA.indx
-unique(AVEL2.df$val[NA.indx])->missingTypes
-table(AVEL2.df$val[NA.indx])->missingTypeTable
-sort(missingTypeTable, decreasing = T)->missingTypeTable
-missingType.df<-data.frame(val=names(missingTypeTable), treatValueAs=NA)
-missingType.List<-lapply(names(missingTypeTable), function(x){intersect(NA.indx, which(x==AVEL2.df$val)) })
-names(missingType.List)<-names(missingTypeTable)
-missingType.info.df<-lapply(missingType.List, function(x){AVEL2.df[x[[1]],]} )
-missingType.info.df<-do.call(rbind, missingType.info.df)
-missingType.info.df$page<-NULL
-missingType.info.df$loc<-NULL
-rownames(missingType.info.df)<-NULL
-missingType.info.df$Example<-NA
-
-n<-1
-setMissing<-function(attr, type, example){
-  n<-which(missingType.info.df$attr==attr)
-  missingType.info.df[n, 7]<<-type
-  missingType.info.df[n, 8]<<-example
-  n<<-n+1
+  setMissing<-function(attr, type, example){
+    n<-which(missingType.info.df$attr==attr)
+    missingType.info.df[n, 7]<<-type
+    missingType.info.df[n, 8]<<-example
+  }
+  
+  
+  setMissing('xml:lang', "string", 'xml:lang=""en-GB"')
+  setMissing('id', "id", 'id="string_wo_colon"')
+  setMissing('class', 'wsp-list', '(just called "list" in documentationclass="info attr-def"')
+  setMissing('style', 'cln-scln-list', '(named list?) style="fill: red; stroke: blue; stroke-width: 3"')
+  #
+  setMissing('requiredExtensions', 'wsp-list', 'list of IRI references: http://example.com/requiredExtension1.svg http://example.com/requiredExtension2.svg')
+  setMissing('requiredFeatures','wsp-list', 'list of feature strings: http://www.w3.org/TR/SVG11/feature#CoreAttribute')
+  setMissing("systemLanguage", 'cmm-list', 'comma-separated list of language names:systemLanguage="mi, en"')
+  setMissing("xlink:arcrole",  'string', ' http://www.example.org/D<c3><bc>rst')
+  #
+  setMissing("xlink:role",  'string', ' http://www.example.org/D<c3><bc>rst')
+  setMissing("xlink:title",  'string', ' http://www.example.org/D<c3><bc>rst')
+  setMissing("xlink:type",  'string', ' http://www.example.org/D<c3><bc>rst')
+  setMissing("xlink:actuate",  'string', 'xlink:actuate = "onLoad"')
+  
+  setMissing("transform", "transform-list", "TODO!!!!!!!!")
+  setMissing("result", "string", '<feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur" />')
+  setMissing("horiz-adv-x", "number", "??")
+  setMissing("keySplines", "cmm-scln-list", 'keySplines="0,0.5,0.5,1; 0.5,0,1,0.5; 0,0.5,0.5,1; 0,0.5,0.5,1" ')
+  #‘keyPoints’ takes a semicolon-separated list of floating point values between 0 and 1
+  setMissing("keyPoints", "scln-list", 'in doc: <list-of-numbers>, a semicolon-separated list of floating point values between 0 and 1 : keyPoints="0; 0.5; 1"')
+  # appears that space-semicolon seperated list will also work
+  # calcMode="spline" keySplines="0 0 1 1; 0 0 1 1" 
+  setMissing("keyTimes", "cmm-scln-list", 'keySplines="0,0.5,0.5,1; 0.5,0,1,0.5; 0,0.5,0.5,1; 0,0.5,0.5,1" ')
+  setMissing("bbox", "cmm-list {4}",
+             'comma-separated list of exactly four numbers specifying, in order, the lower left x, lower left y, upper right x, and upper right y of the bounding box for the complete font'
+  )
+  setMissing("viewBox", "cmm-list {4}", '(<min-x>, <min-y>, <width> and <height>): viewBox="0 0 1500 1000"' )
+  setMissing("begin", "scln-list", 'beginValueList')
+  
+  setMissing('end', "scln-list", "'end-value-list'")
+  setMissing('preserveAspectRatio','special-string', 'preserveAspectRatio="[defer] <align> [<meetOrSlice>]"')
+  setMissing('g1', "cmm-list", 'equence (comma-separated) of glyph names')
+  setMissing("attributeName","string",'ex: attributeName="bar"')
+  setMissing("contentStyleType","string", 'Identifies the default style sheet language: ex: contentStyleType = "text/css"')
+  setMissing("d", "path-data-list", "ToDO!!!!! path-data")
+  setMissing("path", "path-data-list", "ToDO!!!!! path-data")
+  
+  setMissing("panose-1", "wsp-list {10}", "The Panose-1 number, consisting of ten decimal integers, separated by whitespace" )
+  setMissing("kernelUnitLength" , "number-optional-number", "TODO !!!!!!!!!!!!!!")
+  setMissing("lang", "cmm-list", "comma-separated list of language names" )
+  setMissing("xlink:actuate", "choice","for <a> must be 'onRequest', for rest is 'onLoad'")
+  
+  setMissing("media", "cmm-list", "comma-seperated list of media-descriptors")
+  setMissing("xlink:href", "iri", "iri")
+  setMissing("viewTarget", "string", 'viewTarget = "XML_Name [XML_NAME]*"')
+  setMissing("to","string", "in doc is <value>")
+  setMissing("baseProfile", "string", 'Describes the minimum SVG language profile that the author believes is necessary to correctly render the content: ex "none"')
+  
+  setMissing("target", "choice", 'target = "_replace" | "_self" | "_parent" | "_top" | "_blank" | "<XML-Name>"')
+  setMissing('kernelMatrix', 'wsp-list', 'the list of <number>s that make up the kernel matrix for the convolution. Values are separated by space characters and/or a comma. The number of entries in the list must equal <orderX> times <orderY>.'
+  ) # dim specified by order, hmmm! maybe we should overload the order!!
+  setMissing("origin",  "defalut", "literally the string default, has no effect in SVG!!!")
+  setMissing("type", "string", 'defaluts to "text/css"')
+  setMissing("title", "string", 'example: <A href="http://someplace.com/neatstuff.gif" title="Me scuba diving" me scuba diving last summer </A> ') 
+  #may provide tooltip
+  setMissing("name",  "string", "color profile: complicated???")
+  setMissing('by', 'string', 'a single value')
+  setMissing('unicode-range', 'cmm-list',
+             'list of comma seperated unicodes, unicode-range: U+26               /* single_codepoint */
+  unicode-range: U+0025-00FF        /* codepoint_range */
+  unicode-range: U+4??              /* wildcard_range */
+  unicode-range: U+0025-00FF, U+4??
+  ')
+  missingType.info.df
 }
 
-#problems from here down
-#1
-setMissing('xml:lang', "string", 'xml:lang=""en-GB"')
-setMissing('id', "string", 'id="string_wo_colon"')
-setMissing('class', 'wsp-list', '(just called "list" in documentationclass="info attr-def"')
-setMissing('style', 'cln-scln-list', '(named list?) style="fill: red; stroke: blue; stroke-width: 3"')
-#5
-setMissing('requiredExtensions', 'wsp-list', 'list of IRI references: http://example.com/requiredExtension1.svg http://example.com/requiredExtension2.svg')
-setMissing('requiredFeatures','wsp-list', 'list of feature strings: http://www.w3.org/TR/SVG11/feature#CoreAttribute')
-setMissing("systemLanguage", 'cmm-list', 'comma-separated list of language names:systemLanguage="mi, en"')
-setMissing("xlink:arcrole",  'string', ' http://www.example.org/D<c3><bc>rst')
-#9
-setMissing("xlink:role",  'string', ' http://www.example.org/D<c3><bc>rst')
-setMissing("xlink:title",  'string', ' http://www.example.org/D<c3><bc>rst')
-setMissing("xlink:type",  'string', ' http://www.example.org/D<c3><bc>rst')
-setMissing("xlink:actuate",  'string', 'xlink:actuate = "onLoad"')
+missing.df<-getMissingInfo(AVEL2.df)
 
-setMissing("transform", "transform", "TODO!!!!!!!!")
-setMissing("result", "string", '<feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur" />')
-setMissing("horiz-adv-x", "number", "??")
-setMissing("keySplines", "cmm-scln-list", 'keySplines="0,0.5,0.5,1; 0.5,0,1,0.5; 0,0.5,0.5,1; 0,0.5,0.5,1" ')
-#‘keyPoints’ takes a semicolon-separated list of floating point values between 0 and 1
-setMissing("keyPoints", "scln-list", 'in doc: <list-of-numbers>, a semicolon-separated list of floating point values between 0 and 1 : keyPoints="0; 0.5; 1"')
-# appears that space-semicolon seperated list will also work
-# calcMode="spline" keySplines="0 0 1 1; 0 0 1 1" 
-setMissing("keyTimes", "cmm-scln-list", 'keySplines="0,0.5,0.5,1; 0.5,0,1,0.5; 0,0.5,0.5,1; 0,0.5,0.5,1" ')
+which(is.na(AVEL2.df$treatValueAs))->NA.indx
 
-setMissing("bbox", "cmm-4list",
-'comma-separated list of exactly four numbers specifying, in order, the lower left x, lower left y, upper right x, and upper right y of the bounding box for the complete font'
-)
-setMissing("viewBox", "cmm-4list", '(<min-x>, <min-y>, <width> and <height>): viewBox="0 0 1500 1000"' )
+for(n in 1:nrow(missing.df)){
+  mrow<-missing.df[n,]
+  val<-mrow$val
+  which(AVEL2.df$val==val)->val.indx
+  r.indx<-intersect(val.indx, NA.indx)
+  treatAs<-mrow$treatValueAs
+  AVEL2.df[r.indx,9]<-treatAs  
+}
 
-setMissing("begin", "scln-list", 'beginValueList')
 
-setMissing('end', "scln-list", "'end-value-list'")
-setMissing('preserveAspectRatio','special-string', 'preserveAspectRatio="[defer] <align> [<meetOrSlice>]"')
-setMissing('g1', "cmm-list", 'equence (comma-separated) of glyph names')
-setMissing("attributeName","string",'ex: attributeName="bar"')
-setMissing("contentStyleType","string", 'Identifies the default style sheet language: ex: contentStyleType = "text/css"')
-setMissing("d", "path-data", "ToDO!!!!! path-data")
-setMissing("path", "path-data", "ToDO!!!!! path-data")
 
-setMissing("panose-1", "ws-list{10}", "The Panose-1 number, consisting of ten decimal integers, separated by whitespace" )
-setMissing("kernelUnitLength" , "number-optional-number", "TODO !!!!!!!!!!!!!!")
+head(AVEL2.df)
+# indx<-is.na(missingType.info.df$treatValueAs)
+# na.df<-missingType.info.df[indx,]
 
-setMissing("lang", "cmm-list", "comma-separated list of language names" )
-setMissing("xlink:actuate", "choice","for <a> must be 'onRequest', for rest is 'onLoad'")
 
-setMissing("media", "cmm-list", "comma-seperated list of media-descriptors")
 
-setMissing("xlink:href", "iri", "iri")
-setMissing("viewTarget", "string", 'viewTarget = "XML_Name [XML_NAME]*"')
-setMissing("to","string", "in doc is <value>")
-setMissing("baseProfile", "string", 'Describes the minimum SVG language profile that the author believes is necessary to correctly render the content: ex "none"')
-
-setMissing("target", "choice", 'target = "_replace" | "_self" | "_parent" | "_top" | "_blank" | "<XML-Name>"')
 # missingType.info.df[1, 9]<-"String"
 # missingType.info.df[2,10]<-'xml:lang=""en-GB"'
 # 
@@ -299,6 +330,8 @@ setMissing("target", "choice", 'target = "_replace" | "_self" | "_parent" | "_to
 # missingType.info.df[5,10]<-'style="fill: red; stroke: blue; stroke-width: 3"'
 # missingType.info.df[5,9]<-'named-list'
 # 
+
+
 
 
 
