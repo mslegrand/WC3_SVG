dc7158591056c6eaf0bef5b907a143c57857fe5f
sup <legrand@coanalytics.net>
2014-10-16 16:44:20 -0400

    Now writes some unicodes to file

--- getUnicodeTables.R
+++ getUnicodeTables.R
@@ -19,7 +19,16 @@ val.h2<-str_trim(val.h2)
 val.h2<-val.h2[-length(val.h2)]
 val.h2[89]<-"Specials2"
 
-
 names(codeTables)<-val.h2
 names(codeTables)
 str(codeTables)
+
+
+writeToFile<-function(cname, ctable){
+  write.csv(ctable,paste0("Unicode/", cname,".csv"), row.names=F)
+}
+
+toWrite<-c("Greek", "Hebrew", "Superscripts and Subscripts", "Mathematical Operators",
+           "Miscellaneous Technical", "Dingbats", "Arrows")
+
+sapply(toWrite, function(nm){ writeToFile(nm, codeTables[[nm]]) } )
